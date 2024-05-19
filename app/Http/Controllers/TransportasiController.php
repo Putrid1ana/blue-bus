e8202ecc4ecd4b32f001e41a667180485269d0b4
Putrid1ana <spdmha@gmail.com>
2024-05-19 20:12:35 +0700

    push laravel

---
+++ app/Http/Controllers/TransportasiController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Transportasi;
+use Illuminate\Http\Request;
+
+class TransportasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = Category::orderBy('name')->get();
+        $transportasi = Transportasi::with('category')->orderBy('kode')->orderBy('name')->get();
+        return view('server.transportasi.index', compact('category', 'transportasi'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+{
+    $this->validate($request, [
+        'name' => 'required',
+        'kode' => 'required',
+        'jumlah' => 'required|integer',
+        'sisa_kursi' => 'required|integer',
+        'category_id' => 'required'
+    ]);
+
+    Transportasi::updateOrCreate(
+        [
+            'id' => $request->id
+        ],
+        [
+            'name' => $request->name,
+            'kode' => strtoupper($request->kode),
+            'jumlah' => $request->jumlah,
+            'sisa_kursi' => $request->sisa_kursi,
+            'category_id' => $request->category_id,
+        ]
+    );
+
+    if ($request->id) {
+        return redirect()->route('transportasi.index')->with('success', 'Success Update Transportasi!');
+    } else {
+        return redirect()->back()->with('success', 'Success Add Transportasi!');
+    }
+}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+{
+    $category = Category::orderBy('name')->get();
+    $transportasi = Transportasi::find($id);
+    return view('server.transportasi.edit', compact('category', 'transportasi'));
+}
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function update(Request $request, $id)
+{
+    $this->validate($request, [
+        'name' => 'required',
+        'kode' => 'required',
+        'jumlah' => 'required|integer',
+        'sisa_kursi' => 'required|integer',
+        'category_id' => 'required'
+    ]);
+
+    $transportasi = Transportasi::find($id);
+    $transportasi->update([
+        'name' => $request->name,
+        'kode' => strtoupper($request->kode),
+        'jumlah' => $request->jumlah,
+        'sisa_kursi' => $request->sisa_kursi,
+        'category_id' => $request->category_id,
+    ]);
+
+    return redirect()->route('transportasi.index')->with('success', 'Success Update Transportasi!');
+}
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Transportasi::find($id)->delete();
+        return redirect()->back()->with('success', 'Success Delete Transportasi!');
+    }
+}
---
+++ app/Models/Transportasi.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transportasi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'kode',
+        'jumlah',
+        'sisa_kursi',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'category_id');
+    }
+
+    public function pemesanan()
+    {
+        return $this->hasMany('App\Models\Pemesanan', 'transportasi_id');
+    }
+
+    public function hitungSisaKursi()
+    {
+        // Hitung jumlah total kursi yang sudah dipesan
+        $jumlahDipesan = $this->pemesanan()->count();
+
+        // Hitung sisa kursi
+        $sisaKursi = $this->jumlah - $jumlahDipesan;
+
+        return $sisaKursi;
+    }
+
+    protected $table = 'transportasi';
+}
